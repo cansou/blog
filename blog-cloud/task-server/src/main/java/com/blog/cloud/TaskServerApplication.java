e4d0b42887f198de5983da319e895d114eda879e
luoyc <luoyc@anfawuliu.com>
2019-01-31 14:08:59 +0800

    no message

---
+++ blog-cloud/task-server/src/main/java/com/blog/cloud/TaskServerApplication.java
@@ -0,0 +1,21 @@
+package com.blog.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableEurekaClient
+@ServletComponentScan
+@EnableHystrix
+@SpringCloudApplication
+public class TaskServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServerApplication.class, args);
+    }
+
+}
+
