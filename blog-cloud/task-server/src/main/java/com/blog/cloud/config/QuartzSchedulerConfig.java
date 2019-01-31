a5c843c006dacbe69d1cdd11e6b9f22057d45bd3
luoyc <luoyc@anfawuliu.com>
2019-01-31 15:22:59 +0800

    no message

---
+++ blog-cloud/task-server/src/main/java/com/blog/cloud/config/QuartzSchedulerConfig.java
@@ -0,0 +1,116 @@
+package com.blog.cloud.config;
+
+import com.blog.cloud.jobs.WhchatRobotSyncTaskJob;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDetail;
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ */
+@Slf4j
+@Configuration
+public class QuartzSchedulerConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    private static final String QUARTZ_PROPERTIES_NAME = "/quartz.properties";
+
+    @Bean
+    public JobFactory jobFactory(ApplicationContext applicationContext) {
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+
+    /**
+     * 配置Scheduler
+     * @param jobFactory
+     * @param cronTrigger
+     * @param jobDetails
+     * @return
+     */
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory) {
+        SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
+        try {
+            factoryBean.setQuartzProperties(quartzProperties());
+            factoryBean.setDataSource(dataSource);
+            factoryBean.setJobFactory(jobFactory);
+//            factoryBean.setTriggers(cronTrigger);
+//            factoryBean.setJobDetails(jobDetails);
+            factoryBean.setOverwriteExistingJobs(true);
+        } catch (Exception e) {
+            log.error("加载 {} 配置文件失败.", QUARTZ_PROPERTIES_NAME, e);
+            throw new RuntimeException("加载配置文件失败", e);
+        }
+
+        return factoryBean;
+    }
+
+    @Bean(name = "job1Trigger")
+    public CronTriggerFactoryBean job1Trigger(@Qualifier("job1Detail") JobDetail jobDetail) {
+        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+        cronTriggerFactoryBean.setJobDetail(jobDetail);
+        cronTriggerFactoryBean.setCronExpression("0/15 * * * * ?");
+        return cronTriggerFactoryBean;
+    }
+
+    @Bean(name = "job1Detail")
+    public JobDetailFactoryBean job1Detail() {
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        jobDetailFactoryBean.setJobClass(WhchatRobotSyncTaskJob.class);
+        jobDetailFactoryBean.setDurability(true);
+        return jobDetailFactoryBean;
+    }
+
+    /**
+     * 读取quartz 配置文件
+     * @return
+     * @throws IOException
+     */
+    @Bean
+    public Properties quartzProperties() throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource(QUARTZ_PROPERTIES_NAME));
+        propertiesFactoryBean.afterPropertiesSet();
+        return propertiesFactoryBean.getObject();
+    }
+
+    class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+        private transient AutowireCapableBeanFactory beanFactory;
+
+        @Override
+        public void setApplicationContext(final ApplicationContext context) {
+            beanFactory = context.getAutowireCapableBeanFactory();
+        }
+
+        @Override
+        protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+            final Object job = super.createJobInstance(bundle);
+            beanFactory.autowireBean(job);
+            return job;
+        }
+
+    }
+}
