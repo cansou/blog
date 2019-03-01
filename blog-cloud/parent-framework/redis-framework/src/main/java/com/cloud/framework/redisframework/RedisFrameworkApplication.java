e79e87300dc059d981abefd6e9d1f126ea825026
luoyc <luoyc@anfawuliu.com>
2019-03-01 18:06:57 +0800

    no message

---
+++ blog-cloud/parent-framework/redis-framework/src/main/java/com/cloud/framework/redisframework/RedisFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.framework.redisframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisFrameworkApplication.class, args);
+	}
+
+}
---
+++ blog-cloud/parent-framework/redis-framework/src/test/java/com/cloud/framework/redisframework/RedisFrameworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.framework.redisframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisFrameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
